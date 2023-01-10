652f055403575ac91c1ba8d06113f6bd5cf2e100
AndreiRadchenko <d4100kx@gmail.com>
2023-01-10 02:18:36 +0200

    update browserslist

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="goit-react-hw-08-phonebook">
       <App />
-      <GlobalStyle />
     </BrowserRouter>
+    <GlobalStyle />
   </React.StrictMode>
 );
